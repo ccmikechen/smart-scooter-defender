0cdaabdbbfade7a2b051afde0d7d277a60f519d5
mingjia <rm2k3ru6ru6@outlook.com>
2017-02-17 19:18:37 +0800

    sigfox

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ var linebot = require('linebot');
 var express = require('express');
 var app = express();
 
+var myuserid = "U362136ce7ef87de62bae7b85de8b5d7f";
+
 var bot = linebot({
     channelId: "1483029082",
     channelSecret: "ada6218baf7780aea777e87de0b5c093",
     channelAccessToken: "HgmqnvzXuSTtXjB/3yiI0TRFOD2JWImFRFhHavnhGvBLfTeDzBdEREYYWb+oT8zsTfxIBvP4JHHT8kQP853zcJ870pGvKDyKc+zi4cd3ebkitUY8xUa9dkFpIjDjvdNXO5AfquvILjUZ2FLxSluXrwdB04t89/1O/w1cDnyilFU="
 });
 
+app.get('/sigfox', function(req, res) {
+	bot.push(myuserid, req.query.data);
+	res.end();
+});
+
 app.get('/', function (req, res) {
-	
     res.send('Hello World!');
 });
 
